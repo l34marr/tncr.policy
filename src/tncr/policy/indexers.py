4e88c3d09de36bb69962e820f220a291d4151711
TsungWei Hu <marr.tw@gmail.com>
2016-04-01 11:15:34 +0800

    Update Name for District Indexer.

--- src/tncr/policy/indexers.py
+++ src/tncr/policy/indexers.py
@@ -3,11 +3,11 @@ from tncr.content.interfaces import IMonument
 
 
 @indexer(IMonument)
-def district(obj):
+def dstrct(obj):
     try:
         head = obj.address.split(u'\u5340')[0]
-        dist = head.split(u'\u5e02')[1]
+        dstr = head.split(u'\u5e02')[1]
     except:
         return 'Unknown'
-    return dist + u'\u5340'
+    return dstr + u'\u5340'
 
