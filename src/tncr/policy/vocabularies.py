6842869d91d6d18cd0acd84742000ad78b2e8922
TsungWei Hu <marr.tw@gmail.com>
2016-04-02 22:58:37 +0800

    Add Vocabulary for Cultural Resource Types.

---
+++ src/tncr/policy/vocabularies.py
@@ -0,0 +1,25 @@
+from eea.faceted.vocabularies.utils import IVocabularyFactory
+from zope.interface import implements
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.vocabulary import SimpleTerm
+from tncr.policy import _
+
+
+class CrTypes(object):
+    """Vocabulary Factory for Cultural Resource Types.
+    """
+    implements(IVocabularyFactory)
+
+    def __call__(self, context=None):
+        items = (
+            SimpleTerm(value='Monument', title=_(u'Monument')),
+            SimpleTerm(value='Relic', title=_(u'Relic')),
+            SimpleTerm(value='Ruin', title=_(u'Ruin')),
+            SimpleTerm(value='Landscape', title=_(u'Landscape')),
+            SimpleTerm(value='Settlement', title=_(u'Settlement')),
+            SimpleTerm(value='Temple', title=_(u'Temple')),
+            SimpleTerm(value='House', title=_(u'House')),
+            SimpleTerm(value='Tree', title=_(u'Tree'))
+        )
+        return SimpleVocabulary(items)
+
