be5f44f3f67a8fb99c7eeb6bea9b1dd0f1d1ee37
TsungWei Hu <marr.tw@gmail.com>
2016-04-06 10:59:16 +0800

    Update District Indexer for Types.

--- src/tncr/policy/indexers.py
+++ src/tncr/policy/indexers.py
@@ -1,8 +1,69 @@
 from plone.indexer.decorator import indexer
 from tncr.content.interfaces import IMonument
+from tncr.content.interfaces import IRelic
+from tncr.content.interfaces import IRuin
+from tncr.content.interfaces import ILandscape
+from tncr.content.interfaces import ISettlement
+from tncr.content.interfaces import ITemple
+from tncr.content.interfaces import IHouse
+from tncr.content.interfaces import ITree
 
 
 @indexer(IMonument)
+def dstrct(obj):
+    try:
+        head = obj.address.split(u'\u5340')[0]
+        dstr = head.split(u'\u5e02')[1]
+    except:
+        return 'Unknown'
+    return dstr + u'\u5340'
+
+@indexer(IRelic)
+def dstrct(obj):
+    try:
+        head = obj.address.split(u'\u5340')[0]
+        dstr = head.split(u'\u5e02')[1]
+    except:
+        return 'Unknown'
+    return dstr + u'\u5340'
+
+@indexer(IRuin)
+def dstrct(obj):
+    try:
+        head = obj.address.split(u'\u5340')[0]
+        dstr = head.split(u'\u5e02')[1]
+    except:
+        return 'Unknown'
+    return dstr + u'\u5340'
+
+@indexer(ILandscape)
+def dstrct(obj):
+    try:
+        head = obj.address.split(u'\u5340')[0]
+        dstr = head.split(u'\u5e02')[1]
+    except:
+        return 'Unknown'
+    return dstr + u'\u5340'
+
+@indexer(ITemple)
+def dstrct(obj):
+    try:
+        head = obj.address.split(u'\u5340')[0]
+        dstr = head.split(u'\u5e02')[1]
+    except:
+        return 'Unknown'
+    return dstr + u'\u5340'
+
+@indexer(IHouse)
+def dstrct(obj):
+    try:
+        head = obj.address.split(u'\u5340')[0]
+        dstr = head.split(u'\u5e02')[1]
+    except:
+        return 'Unknown'
+    return dstr + u'\u5340'
+
+@indexer(ITree)
 def dstrct(obj):
     try:
         head = obj.address.split(u'\u5340')[0]
